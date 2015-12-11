64c14cf671f07362fa7e89e681f675b817601641
Amaan Cheval <amaan.cheval@gmail.com>
2015-12-11 18:11:24 +0530

    Added helper function to connect 2 nodes

--- map/map.c
+++ map/map.c
@@ -21,7 +21,11 @@ typedef struct NodeStruct {
     // Co-ordinates are relative to the "start" block
     int x;
     int y;
+    // Length of the array called "connected" below
     int num_connections;
+    // So we can easily "push" more nodes using the function. Just counts how many connections have been made already
+    int counter;
+
     // An array of "connection" structure pointers. As seen above, every connection has a cost associated with it (in time)
     // and a pointer to a node.
     // So, for example, node A might be connected to B, C, D.
@@ -56,18 +60,31 @@ Node* CreateNode(x, y, num_connections) {
     return new_node;
 }
 
+void connectNodes(Node *a, Node *b, int cost) {
+    int a_count = a->counter;
+    int b_count = b->counter;
+
+    a->connected[a_count]->ptr = b; // a ---> b
+    b->connected[b_count]->ptr = a; // a <--> b
+    a->connected[a_count]->cost = b->connected[b_count]->cost = cost; // a <---cost---> b
+
+    a->counter++;
+    b->counter++;
+}
+
 int main() {
     Node *start, *r1;
+    // Initialize the 2 nodes with their x, y, and number of connected nodes
     start = CreateNode(0, 0, 1);
     r1 = CreateNode(20, 20, 1);
 
-    start->connected[0]->ptr = r1;
-    r1->connected[0]->ptr = start;
-    r1->connected[0]->cost = start->connected[0]->cost = 10;
-    connectNodes(start, r1, cost);
+    // Actually create the connections between the nodes with the cost at the edge
+    connectNodes(start, r1, 10);
 
-
-    printf("Cost: %d\n", start->connected[0]->cost);
+    // Just to make sure it worked
+    printf("start to r1 cost: %d\n", start->connected[0]->cost);
+    printf("r1 to start cost: %d\n", start->connected[0]->ptr->connected[0]->cost);
+    printf("start.x: %d\n", start->x);
     printf("r1.x: %d\n", start->connected[0]->ptr->x);
     return 0;
 }
