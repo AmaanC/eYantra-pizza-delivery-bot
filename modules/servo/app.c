cfc14e93fa10bcba59e587a0d3e791f91b2ad8d4
Amaan Cheval <amaan.cheval@gmail.com>
2016-01-31 11:12:37 +0530

    Just testing it out

--- modules/servo/app.c
+++ modules/servo/app.c
@@ -4,21 +4,15 @@
 #include "servo.h"
 #include "../buzzer/buzzer.h"
 
-//Main function
-void main(void)
-{
- servo_init_devices();
+void main() {
+    servo_init_devices();
 
- servo_1_move(0, 90);
- _delay_ms(1000);
- servo_1_move(90, 0);
- _delay_ms(1000);
-
- servo_servo_1_free();
-
- buzzer_on();
- _delay_ms(1000);
- buzzer_off();
-
- while(1);
+    servo_servo_1(90);
+    _delay_ms(1000);
+    servo_servo_1(0);
+    while(1) {
+        // _delay_ms(5000);
+        // servo_1_move(90, 0);
+        // _delay_ms(5000);
+    }
 }
--- modules/servo/servo.c
+++ modules/servo/servo.c
@@ -3,6 +3,9 @@
 #include <util/delay.h>
 #include "servo.h"
 
+// The servos have variables storing their position and this position is assigned to OCRnXL using
+// an ISR. The move_to_deg(servoNum, deg) function will allow this variable to be changed.
+
 //Configure PORTB 5 pin for servo motor 1 operation
 void servo_servo1_pin_config (void)
 {
@@ -32,11 +35,46 @@ void servo_port_init(void)
  servo_servo3_pin_config(); //Configure PORTB 7 pin for servo motor 3 operation  
 }
 
+/*
+And so we begin.
+Welcome to Zombo. You can do anything. Welcome! To ZOMBOCOM!
+
+Refer to somewhere in the vicinity of page 120 of the ATMEGA2560 user guide
+for more detailed references.
+
+Here's my understanding, though:
+
+"The 8-bit comparator continuously compares TCNT0 with the Output Compare Registers
+(OCR0A and OCR0B)"
+When they match, it triggers an interrupt.
+
+Keep that in mind.
+
+The timer TOP value controls the frequency of the wave.
+The OCRnx values determine when the PWM's pulse is dropped to 0. (Refer to the diagram)
+So if OCRnx is set to 0x0000, it'll give a 0% duty cycle.
+If OCRnx is set to TOP (0x03FF), it'll give a 100% duty cycle.
+
+TCNT1 below counts from 0xFC01 to 0xFFFF. When it matches 0x03FF (our TOP values),
+it is reset and starts counting again.
+We control the servos by modifying the respective servos' OCR1X (lower nibble) values.
+
+Read section 16.7.3 about fast PWM mode
+And section 16.9.1
+And section 17.11.1
+
+We set COM1A1=1 and COM1A0=0
+According to the table, this clears OC0 on output compare match
+Which is why the servos probably reset to 0 degrees every cycle.
+
+Our WGM=111, so the TOP value for the timer is 0x03FF (according to table 17-2, p148, entry 7)
+*/
+
 //TIMER1 initialization in 10 bit fast PWM mode  
 //prescale:256
 // WGM: 7) PWM 10bit fast, TOP=0x03FF
 // actual value: 52.25Hz 
-void servo_timer1_init(void)
+void servo_timer_init(void)
 {
  TCCR1B = 0x00; //stop
  TCNT1H = 0xFC; //Counter high value to which OCR1xH value is to be compared with
@@ -51,9 +89,26 @@ void servo_timer1_init(void)
  ICR1L  = 0xFF;
  TCCR1A = 0xAB; /*{COM1A1=1, COM1A0=0; COM1B1=1, COM1B0=0; COM1C1=1 COM1C0=0}
  					For Overriding normal port functionality to OCRnA outputs.
+                    Clears the OCnA/B/C on compare match
 				  {WGM11=1, WGM10=1} Along With WGM12 in TCCR1B for Selecting FAST PWM Mode*/
  TCCR1C = 0x00;
  TCCR1B = 0x0C; //WGM12=1; CS12=1, CS11=0, CS10=0 (Prescaler=256)
+
+ // Set up timer 4 for the 4th servo. Output on OC4B, PH4
+ TCCR4B = 0x00;
+ 
+ TCNT4H = 0xFC;
+ TCNT4L = 0x01;
+
+ OCR4BH = 0x03;
+ OCR4BL = 0xFF;
+
+ ICR4H  = 0x03;
+ ICR4L  = 0xFF;
+
+ TCCR4A = 0xAB;
+ TCCR4C = 0x00;
+ TCCR4B = 0x0C;
 }
 
 
@@ -62,7 +117,7 @@ void servo_init_devices(void)
 {
  cli(); //disable all interrupts
  servo_port_init();
- servo_timer1_init();
+ servo_timer_init();
  sei(); //re-enable interrupts 
 }
 
@@ -115,13 +170,4 @@ void servo_servo_3_free (void) //makes servo 3 free rotating
 {
  OCR1CH = 0x03;
  OCR1CL = 0xFF; //Servo 3 off
-}
-
-void servo_1_move(float from_degrees, float to_degrees) {
- int i;
- for (i = from_degrees; i < to_degrees; i++)
- {
-  servo_servo_1(i);
-  _delay_ms(30);
- }
 }
