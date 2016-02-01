8aaacad482937ad728871f26d53308979be9819f
Amaan Cheval <amaan.cheval@gmail.com>
2016-02-01 17:21:53 +0530

    Test all 4 servos

--- modules/servo/app.c
+++ modules/servo/app.c
@@ -8,8 +8,16 @@ void main() {
     servo_init_devices();
 
     servo_1_to(90);
+    servo_2_to(90);
+    servo_3_to(90);
+    servo_4_to(90);
+    
     _delay_ms(1000);
+    
     servo_1_to(0);
+    servo_2_to(0);
+    servo_3_to(0);
+    servo_4_to(0);
     while(1) {
 
     }
