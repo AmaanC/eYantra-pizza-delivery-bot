11fa2610fd6423355d74859cad073104db50f49a
Amaan Cheval <amaan.cheval@gmail.com>
2016-02-01 18:27:04 +0530

    Removed the junk

--- modules/pos_encoder/app.c
+++ modules/pos_encoder/app.c
@@ -12,36 +12,6 @@ int main(void)
 
     while(1)
     {
-        pos_encoder_forward_mm(100); //Moves robot forward 100mm
-        pos_encoder_stop();
-        _delay_ms(500);         
         
-        pos_encoder_back_mm(100);   //Moves robot backward 100mm
-        pos_encoder_stop();         
-        _delay_ms(500);
-        
-        pos_encoder_left_degrees(90); //Rotate robot left by 90 degrees
-        pos_encoder_stop();
-        _delay_ms(500);
-        
-        pos_encoder_right_degrees(90); //Rotate robot right by 90 degrees
-        pos_encoder_stop();
-        _delay_ms(500);
-        
-        pos_encoder_soft_left_degrees(90); //Rotate (soft turn) by 90 degrees
-        pos_encoder_stop();
-        _delay_ms(500);
-        
-        pos_encoder_soft_right_degrees(90); //Rotate (soft turn) by 90 degrees
-        pos_encoder_stop();
-        _delay_ms(500);
-
-        pos_encoder_soft_left_2_degrees(90); //Rotate (soft turn) by 90 degrees
-        pos_encoder_stop();
-        _delay_ms(500);
-        
-        pos_encoder_soft_right_2_degrees(90);   //Rotate (soft turn) by 90 degrees
-        pos_encoder_stop();
-        _delay_ms(500);
     }
 }
--- modules/pos_encoder/pos_encoder.h
+++ modules/pos_encoder/pos_encoder.h
@@ -1,32 +1,8 @@
 #ifndef POS_ENCODER_H_
 #define POS_ENCODER_H_
 
-void pos_encoder_motion_pin_config();
-void left_encoder_pin_config();
-void right_encoder_pin_config();
-void pos_encoder_port_init();
-void left_position_encoder_interrupt_init();
-void right_position_encoder_interrupt_init();
-void pos_encoder_motion_set(unsigned char Direction);
-void pos_encoder_forward();
-void pos_encoder_back();
-void pos_encoder_left();
-void pos_encoder_right();
-void pos_encoder_soft_left();
-void pos_encoder_soft_right();
-void pos_encoder_soft_left_2();
-void pos_encoder_soft_right_2();
-void pos_encoder_stop();
-void pos_encoder_angle_rotate(unsigned int Degrees);
-void pos_encoder_linear_distance_mm(unsigned int DistanceInMM);
-void pos_encoder_forward_mm(unsigned int DistanceInMM);
-void pos_encoder_back_mm(unsigned int DistanceInMM);
-void pos_encoder_left_degrees(unsigned int Degrees) ;
-void pos_encoder_right_degrees(unsigned int Degrees);
-void pos_encoder_soft_left_degrees(unsigned int Degrees);
-void pos_encoder_soft_right_degrees(unsigned int Degrees);
-void pos_encoder_soft_left_2_degrees(unsigned int Degrees);
-void pos_encoder_soft_right_2_degrees(unsigned int Degrees);
 void pos_encoder_init_devices();
+unsigned long int pos_encoder_get_left();
+unsigned long int pos_encoder_get_right();
 
 #endif
