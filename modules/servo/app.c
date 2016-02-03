8a5a53abcd66118c60426ed02e662413ded2afc2
Amaan Cheval <amaan.cheval@gmail.com>
2016-02-03 20:47:58 +0530

    Testing servo with arm

--- modules/servo/app.c
+++ modules/servo/app.c
@@ -7,18 +7,17 @@
 void main() {
     servo_init_devices();
 
-    servo_1_to(90);
-    servo_2_to(90);
-    servo_3_to(90);
-    servo_4_to(90);
-    
-    _delay_ms(1000);
-    
     servo_1_to(0);
     servo_2_to(0);
     servo_3_to(0);
     servo_4_to(0);
+    
+    _delay_ms(3000);
+    
     while(1) {
-
+        servo_1_to(67);
+        servo_2_to(60);
+        servo_3_to(60);
+        servo_4_to(60);
     }
 }
