090ac4e221ded1a92e2d751ac721fb4311604ab8
aswanipranjal <aswani.pranjal@gmail.com>
2016-02-12 18:33:39 +0530

    Fixed Makefile for arrays

--- modules/lcd/app.c
+++ modules/lcd/app.c
@@ -7,15 +7,17 @@
 //Main Function
 int main(void)
 {
-	
 	lcd_init_devices();
 	lcd_set_4bit();
 	lcd_init();
+	_delay_ms(1000);
 	while(1)
 	{
-		lcd_cursor(1,3);
-		lcd_string("FIRE BIRD 5");
-		lcd_cursor(2,1);
-		lcd_string("NEX ROBOTICS IND");
+		lcd_cursor(0,0);
+		lcd_string("DANGER ZONE!");
+		// lcd_wr_char('a');
+		// lcd_wr_char('b');
+		_delay_ms(1000);
+		lcd_wr_command(0x01); //Clear the LCD
 	}
 }
