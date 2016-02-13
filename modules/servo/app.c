f240174a26b01940f0d0762bec528d15b16b3da5
Amaan Cheval <amaan.cheval@gmail.com>
2016-02-13 16:50:48 +0530

    Try all servos as arms

--- modules/servo/app.c
+++ modules/servo/app.c
@@ -7,18 +7,23 @@
 void main() {
     servo_init_devices();
 
-    servo_1_to(20); // Lever
-    servo_2_to(0); // Gripper
-    _delay_ms(2000);
-    servo_1_to(115); // Lower arm
-    _delay_ms(2000);
-    servo_2_to(85); // Close grip
-    
-    servo_3_to(0);
-    servo_4_to(0);
-    
-    while(1) {
-        servo_3_to(60);
-        servo_4_to(60);
+    while (1) {
+        servo_1_to(20); // Lever
+        servo_3_to(20); // Lever
+
+        servo_2_to(0); // Gripper
+        servo_4_to(0); // Gripper
+
+        _delay_ms(2000);
+
+        servo_1_to(115); // Lower arm
+        servo_3_to(115); // Lower arm
+
+        _delay_ms(2000);
+
+        servo_2_to(85); // Close grip
+        servo_4_to(85); // Close grip
+        
+        _delay_ms(5000);
     }
 }
