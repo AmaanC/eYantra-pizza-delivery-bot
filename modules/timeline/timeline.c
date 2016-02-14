921f2b6c13ca436cab7240ac8940eed73d2e3c50
aswanipranjal <aswani.pranjal@gmail.com>
2016-02-14 19:54:53 +0530

    added time blocking function

--- modules/timeline/timeline.c
+++ modules/timeline/timeline.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include "timeline.h"
 #include "../map/map.h"
+#include <math.h>
+
+#define TRUE 1
+#define FALSE 0
 
 // Here's how we plan on solving the order timeline
 // First, you obviously have to enter the order timeline
@@ -29,7 +33,8 @@ Pizza *CreatePizza(
         char size,
         int order_time,
         char order_type,
-        char *delivery_house_name
+        char *delivery_house_name,
+        char status
     ) {
     Pizza *new_pizza;
     new_pizza = malloc(sizeof(Pizza));
@@ -38,11 +43,86 @@ Pizza *CreatePizza(
     new_pizza->order_time = order_time;
     new_pizza->order_type = order_type;
     new_pizza->delivery_house = GetNodeByName(delivery_house_name);
+    new_pizza->status = 'n';
     new_pizza->pickup_point = NULL;
+    if(order_type = 'p'){
+        new_pizza->block->start_time = order_time - 30;
+        new_pizza->block->end_time = order_time + 30;
+    }
+    else
+    {
+        new_pizza->block->start_time = order_time;
+        new_pizza->block->end_time = order_time + 30;
+    }
 
     return new_pizza;
 }
 
+int check_overlap(Time_Block *a, Time_Block *b){
+    if(a->start < b->start && a->end > b->start){
+        return 1;
+    }
+
+    else if(a->start == b->start && a->end == b->end){
+        return 1;
+    }
+
+    else if(a->start > b->start && a->start < b->end){
+        return 1;
+    }
+
+    else 
+        return 0;
+}
+
+void init_timeline(){
+    Pizza **Orders;
+    int o, i;
+    char colour;
+    char size;
+    int order_time;
+    char order_type;
+    char *delivery_house_name;
+    printf("No. of pizzas: ");
+    scanf("%d", &o);
+    Orders = malloc(o * sizeof(Pizza));
+    for(i=0; i<o; i++){
+        printf("pizza: %d", i);
+        scanf("colour: %c\n", &colour);
+        scanf("size: %c\n", &size);
+        scanf("order time: %d\n", &order_time);
+        scanf("order type: %c\n", &order_type);
+        scanf("house: %s\n", delivery_house_name);
+        Orders[i] = CreatePizza(colour, size, order_time, order_type, delivery_house_name);
+    }
+}
+
+void block_time(Pizza **Orders){
+    int i, j, k = 0;
+    int time_threshold = 5;
+    int time_beween;
+    PathStack *Path;
+    float cost_threshold = 5;
+    Blocked_Time **block;
+    block = malloc(10 * sizeof(Blocked_Time));
+    //start time when we pick up pizza from second block
+    //end time when we deliver pizza from any one of blocks
+    //end time will be changed dynamically as soon a s we deliver the pizza
+    for(i=0; i<10; i++){
+        for(j=0; j<10; j++){
+            if(Orders[i]->block->start_time > Orders[j]->block->start_time ){
+                block[k]->start_time = Orders[i]->block->start_time 
+                block[k]->end_time = INFINITY; 
+                k++;
+            }
+            else {
+                block[k]->start_time = Orders[i]->block->start_time;
+                block[k]->end_time = INFINITY;
+                k++;
+            }
+        }
+    }
+}
 void display(Pizza *current_pizza) {
     printf("colour: %c\n", current_pizza->colour);
     printf("size: %c\n", current_pizza->size);
--- modules/timeline/timeline.h
+++ modules/timeline/timeline.h
@@ -8,10 +8,22 @@ typedef struct _Pizza {
     char size; // s, m, l: small, medium, large
     int order_time; // 30, for example
     char order_type; // r, p: regular, preorder
-    Node *delivery_house; // 
-    Node *pickup_point;
+    Node *delivery_house; // it is a node now, not just a name.
+    Node *pickup_point; // this too.
+    char status;
+    //'f' = found
+    //'n' = not found
+    //'d' = delivered
+    //'h' = holding
+    //'c' = canceled
+    Time_Block *block;
 } Pizza;
 
 Pizza *CreatePizza(char colour, char size, int order_time, char order_type, char *delivery_house_name);
+
+typedef struct _Time_Block {
+	int start_time;
+	int end_time;
+}Time_Block;
 
 #endif
