fd6c8f4b5c7a7eede5d3d1a4e319519a193daf1e
aswanipranjal <aswani.pranjal@gmail.com>
2016-02-18 00:04:26 +0530

    updated

--- modules/arm_control/app.c
+++ modules/arm_control/app.c
@@ -8,19 +8,11 @@
 int main() {
 	while(1) {
 		ServoInitDevices();
-		ArmDown(1);
-		ArmDown(2);
-		CloseGripper(1);
-		CloseGripper(2);
-		ArmUp(1);
-		ArmUp(2);
+		PickUpPizza(1);
+		PickUpPizza(2);
 		_delay_ms(5000);
-		ArmDown(1);
-		ArmDown(2);
-		OpenGripper(1);
-		OpenGripper(2);
-		ArmUp(1);
-		ArmUp(2);
+		DepositPizza(1);
+		DepositPizza(2);
 		_delay_ms(5000);
 	}
 
