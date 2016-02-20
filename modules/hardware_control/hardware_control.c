22bc00df76a7bbb2d20a6c087706b8ebe8f2e022
Amaan Cheval <amaan.cheval@gmail.com>
2016-02-20 10:50:53 +0530

    Recompile.

--- modules/hardware_control/hardware_control.c
+++ modules/hardware_control/hardware_control.c
@@ -1,7 +1,6 @@
-
-#include <avr/io.h>
-#include <avr/interrupt.h>
-#include <util/delay.h>
+// #include <avr/io.h>
+// #include <avr/interrupt.h>
+// #include <util/delay.h>
 
 #include <stdio.h>
 #include <stdlib.h>
