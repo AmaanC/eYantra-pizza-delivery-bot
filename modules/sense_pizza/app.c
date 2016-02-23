87edec9e9199ee906c329630eb0f72f8581685d1
aswanipranjal <aswani.pranjal@gmail.com>
2016-02-23 19:37:03 +0530

    final commit from pranjal. Phuck yeah

--- modules/sense_pizza/app.c
+++ modules/sense_pizza/app.c
@@ -3,7 +3,7 @@
 #include <util/delay.h>
 #include "../custom_delay/custom_delay.h"
 #include "../buzzer/buzzer.h"
-#include "../sharp_sensor/SharpSensor.h"
+#include "../sharp_sensor/sharp_sensor.h"
 #include "../color_sensor/color_sensor.h"
 #include "../lcd/lcd.h"
 
--- modules/servo/app.c
+++ modules/servo/app.c
@@ -6,14 +6,14 @@
 
 void main() {
     ServoInitDevices();
-    const int UP_LEVER_1 = 20;
+    const int UP_LEVER_1 = 35;
     const int UP_LEVER_2 = 160;
-    const int DOWN_LEVER_1 = 130;
+    const int DOWN_LEVER_1 = 145;
     const int DOWN_LEVER_2 = 50;
 
     const int OPEN_GRIPPER_1 = 60;
     const int OPEN_GRIPPER_2 = 125;
-    const int CLOSE_GRIPPER_1 = 120;
+    const int CLOSE_GRIPPER_1 = 135;
     const int CLOSE_GRIPPER_2 = 95;
 
     while (1) {
@@ -21,33 +21,24 @@ void main() {
         _delay_ms(1000);
         ServoControl(4, UP_LEVER_2); // Lever up
         _delay_ms(1000);
-
-        ServoControl(2, OPEN_GRIPPER_2); // Gripper open
-        _delay_ms(1000);
-        ServoControl(3, OPEN_GRIPPER_1); // Gripper open
-
-        _delay_ms(1000);
+        //ServoControl(2, OPEN_GRIPPER_2); // Gripper open
+        ////_delay_ms(1000);
+        ////ServoControl(3, OPEN_GRIPPER_1); // Gripper open
+        //_delay_ms(1000);
 
         ServoControl(1, DOWN_LEVER_1); // Lever down
         _delay_ms(1000);
         ServoControl(4, DOWN_LEVER_2); // Lever down
-
         _delay_ms(1000);
 
-        ServoControl(3, CLOSE_GRIPPER_1); // Close grip
-        
+        ServoControl(2, CLOSE_GRIPPER_1); // Close grip
         _delay_ms(3000);
-
-        ServoControl(2, CLOSE_GRIPPER_2); // Close grip
-
+        ServoControl(3, CLOSE_GRIPPER_2); // Close grip
         _delay_ms(3000);
 
         ServoControl(1, UP_LEVER_1); // Lever up
-
         _delay_ms(1000);
-
         ServoControl(4, UP_LEVER_2); // Lever up
-
         _delay_ms(5000);
 
         ServoControl(1, DOWN_LEVER_1); // Lever down
@@ -58,7 +49,6 @@ void main() {
         ServoControl(2, OPEN_GRIPPER_2); // Gripper open
         _delay_ms(1000);
         ServoControl(3, OPEN_GRIPPER_1); // Gripper open
-
         _delay_ms(5000);
     }
 }
