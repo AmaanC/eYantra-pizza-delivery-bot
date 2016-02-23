5c7f3b745003606190a187e20b2d5e09248b5970
Amaan Cheval <amaan.cheval@gmail.com>
2016-02-23 12:04:19 +0530

    Think that should do it

--- modules/timer/app.c
+++ modules/timer/app.c
@@ -3,16 +3,24 @@
 #include <util/delay.h>
 #include "timer.h"
 #include "../buzzer/buzzer.h"
+#include "../lcd/lcd.h"
 
 //Main Function
 int main(void) {
     InitTimer();
-    SevenInitDevices();
-    BuzzerOn();
-    RegisterCallback(BuzzerOff, 2);
-    _delay_ms(4000);
-    FreezeDisplay();
-    _delay_ms(10000);
-    FreezeDisplay();
-    while(1);
+    // SevenInitDevices();
+    LcdInit();
+    // BuzzerOn();
+    RegisterCallback(BuzzerOff, 1000);
+    // _delay_ms(4000);
+    // BuzzerBeep(1000);
+    // FreezeDisplay();
+    // _delay_ms(10000);
+    // FreezeDisplay();
+    LcdPrintf("Hello");
+    _delay_ms(1000);
+    while(1) {
+        LcdPrintf("Time: %d", GetCurrentTime());
+    };
+    return 0;
 }
--- modules/timer/timer.c
+++ modules/timer/timer.c
@@ -5,6 +5,7 @@
 #include "timer.h"
 #include "../buzzer/buzzer.h"
 #include "../seven_segment/seven_segment.h"
+#include "../lcd/lcd.h"
 
 int time_count = 0;
 void (*callback)();
@@ -13,25 +14,25 @@ const int FREEZE_TIME = 5;
 int frozen_time = -5;
 char all_delivered = 0;
 
-// TIMER4 initialize - prescale:1024
+// TIMER3 initialize - prescale:256
 // WGM: 0) Normal, TOP=0xFFFF
 // desired value: 1Hz
 // actual value:  1.000Hz (0.0%)
-void Timer4Init() {
-    TCCR4B = 0x00; // stop
-    TCNT4H = 0x1F; // Counter higher 8 bit value
-    TCNT4L = 0x01; // Counter lower 8 bit value
-    OCR4AH = 0x00; // Output Compair Register (OCR)- Not used
-    OCR4AL = 0x00; // Output Compair Register (OCR)- Not used
-    OCR4BH = 0x00; // Output Compair Register (OCR)- Not used
-    OCR4BL = 0x00; // Output Compair Register (OCR)- Not used
-    OCR4CH = 0x00; // Output Compair Register (OCR)- Not used
-    OCR4CL = 0x00; // Output Compair Register (OCR)- Not used
-    ICR4H  = 0x00; // Input Capture Register (ICR)- Not used
-    ICR4L  = 0x00; // Input Capture Register (ICR)- Not used
-    TCCR4A = 0x00; 
-    TCCR4C = 0x00;
-    TCCR4B = 0x04; // start Timer
+void Timer3Init() {
+    TCCR3B = 0x00; // stop
+    TCNT3H = 0x1F; // Counter higher 8 bit value
+    TCNT3L = 0x01; // Counter lower 8 bit value
+    OCR3AH = 0x00; // Output Compair Register (OCR)- Not used
+    OCR3AL = 0x00; // Output Compair Register (OCR)- Not used
+    OCR3BH = 0x00; // Output Compair Register (OCR)- Not used
+    OCR3BL = 0x00; // Output Compair Register (OCR)- Not used
+    OCR3CH = 0x00; // Output Compair Register (OCR)- Not used
+    OCR3CL = 0x00; // Output Compair Register (OCR)- Not used
+    ICR3H  = 0x00; // Input Capture Register (ICR)- Not used
+    ICR3L  = 0x00; // Input Capture Register (ICR)- Not used
+    TCCR3A = 0x00; 
+    TCCR3C = 0x00;
+    TCCR3B = 0x04; // start Timer
 }
 
 void UpdateDisplay() {
@@ -44,22 +45,46 @@ void UpdateDisplay() {
 }
 
 // This ISR can be used to schedule events like refreshing ADC data, LCD data
-ISR(TIMER4_OVF_vect) {
-    // TIMER4 has overflowed
-    TCNT4H = 0x1F; // reload counter high value
-    TCNT4L = 0x01; // reload counter low value
+ISR(TIMER3_OVF_vect) {
+    // TIMER3 has overflowed
+    TCNT3H = 0x1F; // reload counter high value
+    TCNT3L = 0x01; // reload counter low value
     
     if (all_delivered == 0) {
         time_count++;
     }
+}
+
+// (0xFFFF - (one_sec / 10)).toString(16)
+void Timer5Init() {
+    TCCR5B = 0x00; // stop
+    TCNT5H = 0x9A; // Counter higher 8 bit value
+    TCNT5L = 0xFB; // Counter lower 8 bit value
+    OCR5AH = 0x00; // Output Compair Register (OCR)- Not used
+    OCR5AL = 0x00; // Output Compair Register (OCR)- Not used
+    OCR5BH = 0x00; // Output Compair Register (OCR)- Not used
+    OCR5BL = 0x00; // Output Compair Register (OCR)- Not used
+    OCR5CH = 0x00; // Output Compair Register (OCR)- Not used
+    OCR5CL = 0x00; // Output Compair Register (OCR)- Not used
+    ICR5H  = 0x00; // Input Capture Register (ICR)- Not used
+    ICR5L  = 0x00; // Input Capture Register (ICR)- Not used
+    TCCR5A = 0x00; 
+    TCCR5C = 0x00;
+    TCCR5B = 0x04; // start Timer
+}
+
+ISR(TIMER5_OVF_vect) {
+    TCNT5H = 0x9A;
+    TCNT5L = 0xFB;
 
     if (callback_delay > 0) {
         callback_delay--;
     }
     else if (callback != NULL) {
         callback();
+        callback = NULL;
     }
-    // UpdateDisplay();
+    UpdateDisplay();
 }
 
 void VictoryTime() {
@@ -72,11 +97,12 @@ void FreezeDisplay() {
 
 // Call the registered function after a delay
 char RegisterCallback(void (*fn)(), short int delay) {
-    if (callback == NULL) {
+    if (callback != NULL) {
         // Can't register callback
         return 0;
     }
     else {
+        // LcdPrintf("Registered");
         callback = fn;
         callback_delay = delay;
         return 1;
@@ -93,7 +119,9 @@ void ResetTime() {
 
 void InitTimer() {
     cli(); // Clears the global interrupts
-    Timer4Init();
-    TIMSK4 = 0x01; // timer4 overflow interrupt enable
+    Timer3Init();
+    Timer5Init();
+    TIMSK3 = 0x01; // timer4 overflow interrupt enable
+    TIMSK5 = 0x01;
     sei();   // Enables the global interrupts
 }
