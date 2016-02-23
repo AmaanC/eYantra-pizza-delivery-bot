4ee6ef3a001ddc07a68bf0b041aeaefd3968dcb7
Amaan Cheval <amaan.cheval@gmail.com>
2016-02-23 09:13:21 +0530

    Fixing previous commit where I forgot to add the new files

---
+++ modules/seven_segment/seven_segment.h
@@ -0,0 +1,10 @@
+#ifndef SEVENSEGMENT_H_
+#define SEVENSEGMENT_H_
+
+void SevenSegmentPinConfig();
+void SevenPortInit();
+void SevenInitDevices();
+int SevenConvertToHex(int num);
+void SevenDisplayNum(int num);
+
+#endif
